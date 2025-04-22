b21db39450fc5c3b1d1312a461fd333e00eabba7
calberti <calberti@student.unimelb.edu.au>
2025-04-22 15:55:57 +1000

    feat: add sponsors

--- src/panels/sponsors.tsx
+++ src/panels/sponsors.tsx
@@ -1,7 +1,63 @@
-export default function SponsorsPanel() {
+import SponsorTitle from "@/assets/sponsors-title.png"
+
+import AirwallexIcon from "@/assets/sponsors/airwallex.png"
+import IMCIcon from "@/assets/sponsors/imc.png"
+import JaneStreetIcon from "@/assets/sponsors/jane-street.png"
+
+import AtlassianIcon from "@/assets/sponsors/atlassian.png"
+import TalariaIcon from "@/assets/sponsors/talaria.png"
+import SkandIcon from "@/assets/sponsors/skand.png"
+import OptiverIcon from "@/assets/sponsors/optiver.png"
+import SIGIcon from "@/assets/sponsors/sig.png"
+
+import SuncorpIcon from "@/assets/sponsors/suncorp.png"
+import CitadelIcon from "@/assets/sponsors/citadel.png"
+import KPMGIcon from "@/assets/sponsors/kpmg.png"
+import ShineIcon from "@/assets/sponsors/shine.png"
+
+import CommbankIcon from "@/assets/sponsors/commbank.png"
+import REAIcon from "@/assets/sponsors/rea.png"
+import VanguardIcon from "@/assets/sponsors/vanguard.png"
+import XeroIcon from "@/assets/sponsors/xero.png"
+import QRTIcon from "@/assets/sponsors/qrt.png"
+import EYIcon from "@/assets/sponsors/ey.png"
+
+const tiers = [
+    {
+      name: 'Diamond',
+      logos: [AirwallexIcon, IMCIcon, JaneStreetIcon],
+    },
+    {
+      name: 'Platinum',
+      logos: [AtlassianIcon, TalariaIcon, SkandIcon, OptiverIcon, SIGIcon],
+    },
+    {
+      name: 'Gold',
+      logos: [SuncorpIcon, CitadelIcon, KPMGIcon, ShineIcon],
+    },
+    {
+      name: 'Silver',
+      logos: [CommbankIcon, REAIcon, VanguardIcon, XeroIcon, QRTIcon, EYIcon],
+    },
+  ];
+  
+  export default function SponsorsPanel() {
     return (
-        <div className="flex w-screen h-screen items-center justify-center bg-offblack">
-            <h1 className="text-5xl font-bold font-abduction text-green">Sponsors panel</h1>
-        </div>
+      <div className="px-6 py-24 bg-offblack text-white">
+        <img className="block mx-auto mb-10 h-8 sm:h-12" src={SponsorTitle} alt="Sponsors"></img>
+
+        {tiers.map(({ name, logos }) => (
+          <div key={name} className="flex flex-col sm:flex-row items-center mb-10 justify-center sm:space-x-20">
+            <h3 className="font-abduction text-xl sm:text-2xl mb-6 sm:mb-0">
+              {name}
+            </h3>
+              <div className="flex flex-wrap items-center justify-center gap-6">
+              {logos.map((logo) => (
+                <img src={logo} className="h-5 sm:h-8"/>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     )
-}
+  }
