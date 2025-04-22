74eca66133aa9379239c9b6b9125fd63f4ffeef5
calberti <calberti@student.unimelb.edu.au>
2025-04-22 16:21:40 +1000

    chore: remove unfinished pages from app

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,16 @@
 import LandingPanel from "./panels/landing"
+import AboutPanel from "./panels/about"
 import SponsorsPanel from "./panels/sponsors"
 import ThemesPanel from "./panels/themes"
 import TimelinePanel from "./panels/timeline"
 import WorkshopsPanel from "./panels/workshop"
+import FAQPanel from "./panels/faq"
+
 
 export default function App() {
     return (
         <div>
             <LandingPanel/>
-            <WorkshopsPanel/>
-            <TimelinePanel/>
             <ThemesPanel/>
             <SponsorsPanel/>
         </div>
